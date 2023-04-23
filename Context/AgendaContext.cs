5efe8a12b821f39bd06bc19521b4a76c15fabd23
Paulo Lacerda <contatopaulolacerda@gmail.com>
2023-04-23 10:20:51 -0300

    Criação do Contexto e Confg Conexao

---
+++ Context/AgendaContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using MODULOAPI.Entities;
+
+namespace MODULOAPI.Context
+{
+    public class AgendaContext : DbContext
+    {
+        public AgendaContext(DbContextOptions<AgendaContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Contato> Contatos {get; set;}
+    }
+
+}
