774aab525da483cd9c7ef1e86a215112e8b7201d
Paulo Lacerda <contatopaulolacerda@gmail.com>
2023-04-23 13:15:15 -0300

    CreateAnEndPointForUpdate

--- Controllers/ContatoController.cs
+++ Controllers/ContatoController.cs
@@ -35,5 +35,26 @@ namespace MODULOAPI.Controllers
         return Ok(contato);
 
        }
+
+       [HttpPut("(id)")]
+
+       public IActionResult Update(int id, Contato contato)
+       {
+        var contatoBanco = _context.Contatos.Find(id);
+
+        if (contatoBanco == null)
+            return NotFound();
+
+        contatoBanco.Nome = contato.Nome;
+        contatoBanco.Telefone = contato.Telefone;
+        contatoBanco.Ativo = contato.Ativo;
+
+        _context.Contatos.Update(contatoBanco);
+        _context.SaveChanges();
+
+        return Ok(contatoBanco);
+
+        
+       }
     }
 }
