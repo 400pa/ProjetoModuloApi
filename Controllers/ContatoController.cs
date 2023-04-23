567870828db3a9e971e16922f8e6f6cd5afdb6a1
Paulo Lacerda <contatopaulolacerda@gmail.com>
2023-04-23 12:25:44 -0300

    CreateAControllerAndAnEndPoint

---
+++ Controllers/ContatoController.cs
@@ -0,0 +1,27 @@
+using Microsoft.AspNetCore.Mvc;
+using MODULOAPI.Context;
+using MODULOAPI.Entities;
+
+namespace MODULOAPI.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class ContatoController : ControllerBase
+    {
+        private readonly AgendaContext _context;
+       public ContatoController(AgendaContext context)
+       {
+        
+        _context = context;
+       } 
+
+        [HttpPost]
+       public IActionResult Create(Contato contato)
+       {
+        _context.Add (contato);
+        _context.SaveChanges();
+        return Ok(contato);
+       }
+    }
+}
